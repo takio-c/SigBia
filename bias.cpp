5f95a53171a98a2f7f7b997c605b118be558ae8e
Takio Yamaoka <takio_c@hotmail.com>
2012-11-16 11:39:44 +0900

    editting for cleaning

--- bias.cpp
+++ bias.cpp
@@ -16,70 +16,74 @@ float Normal( void ){
 }
 
 #define VAR(s)	sqrt(var_##s)
-#define STV_NUM	2				// 状態数
+#define STV_NUM	3				// 状態数
 #define MES_NUM	1				// 観測数
 #define Fs		100.0			// サンプリング周期
 #define Ts		(1.0/Fs)		// サンプリング間隔
 #define T		10.0			// 信号周期
 #define OMEGA	(2.0*M_PI/T)	// 信号角速度
 
-double bia[MES_NUM] = {
-	0.8,
-};
 double sig[STV_NUM] = {
-	0.0, 0.0, 
+	0.0,	// signal
+	0.0,	// differencial
+	9.0,	// bias
 };
 double init[STV_NUM] = {
-	0.0, 0.0, 
+	0.0,	// signal
+	0.0,	// differencial
+	0.0,	// bias
 };
 double phi[STV_NUM*STV_NUM] = {
-	1.0, Ts, 
-	0.0, 1.0, 
+	1.0,	Ts,		0.0,	// for signal
+	0.0,	1.0,	0.0,	// for differencial
+	0.0,	0.0,	1.0,	// for bias
 };
 double h[MES_NUM*STV_NUM] = {
-	1.0, 0.0, 
+	1.0,	0.0,	1.0,	// 
 };
 // var
 double var_e[STV_NUM*STV_NUM] = {
-	0.2, 0.0, 
-	0.0, 0.0, 
+	0.0,	0.0,	0.0,	// 
+	0.0,	0.0,	0.0,	// 
+	0.0,	0.0,	1.0,	// 
 };
 double var_w[STV_NUM] = {
-	0.0, 
-	0.0, 
+	0.0,	// on signal
+	0.0,	// on differencial
+	0.0,	// on bias
 };
 double var_v[MES_NUM] = {
-	0.1, 
+	0.1,	// on sensor
 };
 
 int main(int argc, char* argv[])
 {
-	// measure
-	Matrix<double> B(MES_NUM, 1);
+	// true
+	Matrix<double> S(STV_NUM, 1);
+	// estimate
+	Matrix<double> X(STV_NUM, 1);
+	// sensor
 	Matrix<double> H(MES_NUM, STV_NUM);
 	Matrix<double> Z(MES_NUM, 1);
 	Matrix<double> V(MES_NUM, 1);
 	Matrix<double> R(MES_NUM, MES_NUM);
 	// status vector
 	Matrix<double> PHI(STV_NUM, STV_NUM);
-	Matrix<double> S(STV_NUM, 1);
-	Matrix<double> X(STV_NUM, 1);
 	Matrix<double> W(STV_NUM, 1);
 	Matrix<double> Q(STV_NUM, STV_NUM);
-	Matrix<double> P(STV_NUM, STV_NUM);
 	// Kalman
+	Matrix<double> P(STV_NUM, STV_NUM);
 	Matrix<double> K(STV_NUM, MES_NUM);
 
 	// init
-	B.Set(bia);
-	H.Set(h);
 	S.Set(sig);
 	X.Set(init);
+	H.Set(h);
 	PHI.Set(phi);
 	// var
-	P.Set(var_e);
-	Q.Dia(var_w);
 	R.Dia(var_v);
+	Q.Dia(var_w);
+	P.Set(var_e);
 
 	unsigned int loop = 10000;
 	if(1 < argc) loop = atoi(argv[1]);
@@ -93,32 +97,31 @@ int main(int argc, char* argv[])
 		}
 		// set sensor
 		S = PHI * S + W;
-		Z = H * S + B + V;
+		Z = H * S + V;
 		// P previous
 		P = PHI * P * PHI.tra() + Q;
 		// gain
 		K = P * H.tra() * (H * P * H.tra() + R).inv();
 		// update
-		double xd = X[0][0];
 		X = PHI * X + K * (Z - H * PHI * X);
 		// P next
 		P = ( (K * H).ide() - K * H) * P;
 		// output
-		printf("%lf %lf %lf %lf %lf %lf %s",
+		printf("%lf %lf %lf %lf %lf %s",
 				Z[0][0],		// sensor
-				S[0][0],		// signal
-				B[0][0],		// bias
-				X[0][0],		// status signal
+				S[0][0],		// true signal
+				X[0][0],		// estimate signal
 				P[0][0],		// error variance for signal
-				P[1][1],		// error variance for bias
+				P[2][2],		// error variance for bias
 				"\n"
 			  );
 		// status update
-//		if(2500 < i && i <= 5000){
-			S[1][0] = OMEGA*sin(OMEGA*i*Ts);
-			X[1][0] = OMEGA*sin(OMEGA*i*Ts);
-//			X[1][0] = X[0][0] - xd;
-//		}
+#if 0
+		if(2500 < i && i <= 5000){
+			S[1][0] = OMEGA*cos(OMEGA*i*Ts);
+			X[1][0] = OMEGA*cos(OMEGA*i*Ts);
+		}
+#endif
 	}
 
 	return 0;
